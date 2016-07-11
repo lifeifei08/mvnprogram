692abe275e1f447671ea71a9563525888c8eda73
李菲菲 <liff2009@126.com>
2016-07-11 11:47:25 +0800

    上传代码测试

---
+++ src/main/java/com/dp/searchads/MvnDemo.java
@@ -0,0 +1,14 @@
+package com.dp.searchads;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by lifeifei on 16/7/8.
+ */
+public class MvnDemo {
+
+    public static void main(String []args) {
+        String []strArr = {"a", "b", "c"};
+        System.out.println(StringUtils.join(strArr, '\t'));
+    }
+}
